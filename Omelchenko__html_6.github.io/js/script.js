2262d122db73e7e20bb2be997587c8045e4db4a4
Olena <elenika.om@gmail.com>
2017-07-24 14:25:26 +0300

    add js folder, change id to class in js style for tas4, popup table, add svg fill to html instead of css

---
+++ Omelchenko__html_6.github.io/js/script.js
@@ -0,0 +1,5 @@
+function show(state) {
+	'use strict';
+  	document.getElementsByClassName('wrap')[0].style.display = state;
+  	document.getElementsByClassName('popup-item')[0].style.display = state;
+}
